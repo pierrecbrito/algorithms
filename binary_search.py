da255a996fd297ae30a19a9bca07e930ce21a790
Pierre Brito <33200451+pierrecbrito@users.noreply.github.com>
2024-05-02 10:40:37 -0300

    Binary Search

---
+++ binary_search.py
@@ -0,0 +1,23 @@
+import math
+
+def search_in(item, array):
+    lower = 0
+    higher = len(array) - 1
+
+    while(lower <= higher):
+        middle = math.floor((lower + higher) / 2)
+        try_this = array[middle]
+
+        if try_this == item:
+            return middle
+        
+        if try_this > item:
+            higher = middle - 1
+        else:
+            lower = middle + 1
+    
+    return None
+
+list = list(range(1, 100000, 1))
+
+print(search_in(10000, list))
